d3e121b3ca98957c54ffe22a21ce69f16950cc8a
AlliterativeAnchovies <alliterative.anchovies@gmail.com>
2018-10-13 13:37:27 +0100

    Added my block stuff

---
+++ core/src/com/mygdx/kiddiecode/ILikeTextInputHandlers.java
@@ -0,0 +1,20 @@
+package com.mygdx.kiddiecode;
+
+import com.badlogic.gdx.Input;
+
+public class ILikeTextInputHandlers implements Input.TextInputListener {
+    private Block block;
+    private String innerNodeKey;
+
+    @Override
+    public void input (String text) {
+        block.getInnerNodes().put(innerNodeKey,text);
+    }
+
+    @Override
+    public void canceled () {
+    }
+
+    public void setBlock(Block b) {block = b;}
+    public void setInnerNodeKey(String s) {innerNodeKey = s;}
+}
---
+++ core/src/com/mygdx/kiddiecode/Node.java
@@ -0,0 +1,71 @@
+package com.mygdx.kiddiecode;
+
+public class Node {
+    private Block parentBlock;
+
+    private float posX;//position relative to parent block
+    private float posY;//position relative to parent block
+    private boolean isHighlightedVar;
+    private boolean hoverOver;
+    private Node nodeConnectedTo;
+    private boolean isInputNodeVar;
+
+    public Node(Block b,float x,float y,boolean isIncoming) {
+        parentBlock = b;
+        posX = x;
+        posY = y;
+        isHighlightedVar = false;
+        hoverOver = false;
+        nodeConnectedTo = null;
+        isInputNodeVar = isIncoming;
+    }
+
+    public boolean isInputNode() {return isInputNodeVar;}
+
+    public void connectTo(Node n) {
+        if (nodeConnectedTo != null) {nodeConnectedTo.nodeConnectedTo = null;}
+        if (n != null) {
+            if (n.nodeConnectedTo != null) {n.nodeConnectedTo.nodeConnectedTo = null;}
+            n.nodeConnectedTo = this;
+        }
+        nodeConnectedTo = n;
+    }
+
+    public void draw() {
+        if (isHoverOver()) {
+            Block.shupooey.setColor(1, 0.5f, 1, 1);
+        }
+        else if (isHighlighted()) {
+            Block.shupooey.setColor(0, 0, 1, 1);
+        }
+        else {
+            Block.shupooey.setColor(0.5f, 0.5f, 0.5f, 1);
+        }
+        Block.shupooey.circle(posX+parentBlock.getPosX(),Block.progCoord(posY+parentBlock.getPosY()),10);
+        Block.shupooey.setColor(1,1,0,1);
+        if (nodeConnectedTo != null) {
+            Block.shupooey.rectLine(getPosX(), Block.progCoord(getPosY()), nodeConnectedTo.getPosX(), Block.progCoord(nodeConnectedTo.getPosY()),5);
+        }
+    }
+    public boolean isHoverOver() {return hoverOver;}
+    public void makeHoverOver() {hoverOver = true;}
+    public void unmakeHoverOver() {hoverOver = false;}
+    public boolean isHighlighted() {return isHighlightedVar;}
+    public void highlight() {
+        if (!isHighlightedVar) {System.out.println("Highlighting Node");}
+        isHighlightedVar = true;
+    }
+    public void delight() {
+        if (isHighlightedVar) {System.out.println("Delighting Node");}
+        isHighlightedVar = false;
+    }
+    public com.badlogic.gdx.math.Circle boundCircle() {
+        return new com.badlogic.gdx.math.Circle(posX+parentBlock.getPosX(),Block.progCoord(posY+parentBlock.getPosY()),10);
+    }
+
+    public float getPosX() {return posX+parentBlock.getPosX();}
+    public float getPosY() {return posY+parentBlock.getPosY();}
+    public float getRawPosX() {return posX;}
+    public float getRawPosY() {return posY;}
+
+}

d44f7f8937dfb8f6b0b18b1b5326e0b705339fa6
AlliterativeAnchovies <alliterative.anchovies@gmail.com>
2018-10-13 14:48:17 +0100

    ˙∆˚

--- core/src/com/mygdx/game/Main.java
+++ core/src/com/mygdx/game/Main.java
@@ -4,12 +4,20 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.*;
 import com.mygdx.game.input.HandleInput;
 import com.mygdx.game.rendering.MainRenderer;
 import com.mygdx.game.tick.Ticker;
 import com.mygdx.game.world.WorldGrid;
+import com.mygdx.kiddiecode.*;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+
+import java.util.ArrayList;
+
+import static com.mygdx.kiddiecode.MasterClass.shapeRenderer;
 
 public class Main extends ApplicationAdapter {
 
@@ -42,6 +50,25 @@ public class Main extends ApplicationAdapter {
 
         createSphere();
         createFloor();
+
+        //KiddieCode stuff
+        Gdx.input.setInputProcessor(new IREALLYDespiseGestureDetectors(new IHateGestureListeners(this)));
+
+        shapeRenderer = new ShapeRenderer();
+        MasterClass.fontyWonty = new BitmapFont();
+        MasterClass.batch = new SpriteBatch();
+        Block.shupooey = shapeRenderer;
+        Block.spadoogle = MasterClass.batch;
+        Block.biddangger = MasterClass.fontyWonty;
+        MasterClass.blocks = new java.util.ArrayList<Block>();
+        MasterClass.blocks.add(new Block(50,200,BlockTypes.ONLOAD_TRIGGER));
+        MasterClass.blocks.add(new Block(100,350,BlockTypes.IF_LESS_THAN));
+        MasterClass.blocks.add(new Block(150,200,BlockTypes.LOOP_FROM_TO));
+        MasterClass.blocks.add(new Block(200,350,BlockTypes.LOOP_FROM_TO));
+        MasterClass.blocks.add(new Block(250,200,BlockTypes.PLACE_BLOCK));
+        MasterClass.blocks.add(new Block(300,350,BlockTypes.PLACE_PLAYER));
+        MasterClass.blocks.add(new Block(350,200,BlockTypes.ONTICK_TRIGGER));
+        MasterClass.blocks.add(new Block(400,350,BlockTypes.MOVE_PLAYER_BY));
     }
 
     public static void createFloor() {
@@ -96,12 +123,37 @@ public class Main extends ApplicationAdapter {
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(0.4f, 0.4f, 0.4f, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+	    //toggle this to swap between the platformer sandbox and the scripting sandbox
+	    boolean CODEMODE = false;
+	    if (!CODEMODE) {
+            Gdx.gl.glClearColor(0.4f, 0.4f, 0.4f, 1);
+            Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        debugRenderer.render(world, cam.combined);
-        renderer.render();
-        tick.tick();
+            debugRenderer.render(world, cam.combined);
+            renderer.render();
+            tick.tick();
+        }
+        else {
+            Gdx.gl.glClearColor(1, 1, 1, 1);
+            Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+            shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+            for (Block b: MasterClass.blocks) {
+                b.draw();
+            }
+            //Circle TEMP = getAllNodes().get(0).boundCircle();
+            //shapeRenderer.setColor(0,0,1,1);
+            //shapeRenderer.circle(TEMP.x,TEMP.y,TEMP.radius);
+            if (MasterClass.getStartTerminalNode() != null && MasterClass.getStartTerminalNode().isHighlighted()) {
+                //draw line from startTerminalNode.pos to drag.pos
+                shapeRenderer.rectLine(MasterClass.getStartTerminalNode().getPosX(),Block.progCoord(MasterClass.getStartTerminalNode().getPosY()), MasterClass.getDragX(),Block.progCoord(MasterClass.getDragY()),5);
+            }
+            shapeRenderer.end();
+            MasterClass.batch.begin();
+            for (Block b: MasterClass.blocks) {
+                b.drawTheFrigginText();
+            }
+            MasterClass.batch.end();
+        }
 	}
 
     @Override
@@ -115,4 +167,17 @@ public class Main extends ApplicationAdapter {
 	public void dispose () {
 	    renderer.dispose();
 	}
+
+	public ArrayList<Block> getBlocks() {
+	    return MasterClass.blocks;
+    }
+
+    public ArrayList<Node> getAllNodes() {
+        java.util.ArrayList<Node> toReturn = new java.util.ArrayList<Node>();
+        for (Block b : MasterClass.blocks) {
+            toReturn.addAll(b.getIncomingNodes());
+            toReturn.addAll(b.getOutgoingNodes());
+        }
+        return toReturn;
+    }
 }

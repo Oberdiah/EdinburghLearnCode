08a80fc8a56ba8925d6de26816e358a7c082c68e
Oberdiah <oberdead@gmail.com>
2018-10-13 22:47:23 +0100

    Player animation

--- core/src/com/mygdx/game/rendering/MainRenderer.java
+++ core/src/com/mygdx/game/rendering/MainRenderer.java
@@ -3,7 +3,9 @@ package com.mygdx.game.rendering;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 import com.mygdx.game.Main;
 import com.mygdx.game.entites.Entity;
@@ -25,62 +27,62 @@ public class MainRenderer {
 
         Main.cam.position.set(Main.cam.viewportWidth / 2f, Main.cam.viewportHeight / 2f, 0);
         Main.cam.update();
+
+        create();
     }
 
     public void render() {
         Vector2 position = WorldGrid.playerEntity.physicsObject.getPosition();
         Main.cam.position.set(position.x, position.y, 0);
         Main.cam.update();
-
+        stateTime += Gdx.graphics.getDeltaTime();
         batch.setProjectionMatrix(Main.cam.combined);
 
         batch.begin();
 
         renderWorld();
 
-        //renderEntities();
+        renderEntities();
+
+        System.out.println(Gdx.graphics.getFramesPerSecond() + "fps");
 
         batch.end();
     }
 
+    Animation<TextureRegion> walkAnimation;
+    Texture walkSheet;
+    float stateTime;
+
     private void renderEntities() {
         for (Entity e : Main.worldGrid.getEntityArrayList()) {
-            e.sprite.draw(batch);
+            // Get current frame of animation for the current stateTime
+            TextureRegion currentFrame = walkAnimation.getKeyFrame(stateTime, true);
+            Vector2 position = WorldGrid.playerEntity.physicsObject.getPosition();
+            float width = 1;
+            float height = 2;
+            batch.draw(currentFrame, position.x-width/2, position.y-height/2, width/2, height/2, width, height, 1,1,20.0f); // Draw current frame at (50, 50)
         }
     }
 
-    /*
     public void create() {
+        final int FRAME_COLS = 6, FRAME_ROWS = 3;
+        walkSheet = new Texture(Gdx.files.internal("player.png"));
 
-        // Load the sprite sheet as a Texture
-        walkSheet = new Texture(Gdx.files.internal("animation_sheet.png"));
-
-        // Use the split utility method to create a 2D array of TextureRegions. This is
-        // possible because this sprite sheet contains frames of equal size and they are
-        // all aligned.
         TextureRegion[][] tmp = TextureRegion.split(walkSheet,
                 walkSheet.getWidth() / FRAME_COLS,
                 walkSheet.getHeight() / FRAME_ROWS);
 
-        // Place the regions into a 1D array in the correct order, starting from the top
-        // left, going across first. The Animation constructor requires a 1D array.
-        TextureRegion[] walkFrames = new TextureRegion[FRAME_COLS * FRAME_ROWS];
+        TextureRegion[] walkFrames = new TextureRegion[FRAME_COLS];
         int index = 0;
-        for (int i = 0; i < FRAME_ROWS; i++) {
-            for (int j = 0; j < FRAME_COLS; j++) {
-                walkFrames[index++] = tmp[i][j];
-            }
+        for (int j = 0; j < FRAME_COLS; j++) {
+            walkFrames[index++] = tmp[0][j];
         }
 
-        // Initialize the Animation with the frame interval and array of frames
-        walkAnimation = new Animation<TextureRegion>(0.025f, walkFrames);
+        walkAnimation = new Animation<>(0.25f, walkFrames);
 
-        // Instantiate a SpriteBatch for drawing and reset the elapsed animation
-        // time to 0
-        spriteBatch = new SpriteBatch();
         stateTime = 0f;
     }
-*/
+
     private void renderWorld() {
         for (int xSqr = 0; xSqr < WorldGrid.worldWidth; xSqr++)
         {

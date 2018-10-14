fb388fe1ac5f7c664d64227886389684260e5352
Oberdiah <oberdead@gmail.com>
2018-10-14 10:08:51 +0100

    Spawn entity works

--- core/src/com/mygdx/game/Main.java
+++ core/src/com/mygdx/game/Main.java
@@ -164,8 +164,9 @@ public class Main extends ApplicationAdapter {
             Gdx.input.setInputProcessor(stage);
             Gdx.gl.glClearColor(0.4f, 0.4f, 0.4f, 1);
             Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-            PhysicsHandler.render();
             renderer.render();
+            PhysicsHandler.render();
+
             tick.tick();
 
 

c2d71bfb28f6fbd5e5f30b25c515c003df72aabd
Oberdiah <oberdead@gmail.com>
2018-10-14 08:56:26 +0100

    More readable block names

--- core/src/com/mygdx/kiddiecode/BlockTypes.java
+++ core/src/com/mygdx/kiddiecode/BlockTypes.java
@@ -1,15 +1,21 @@
 package com.mygdx.kiddiecode;
 
 public enum BlockTypes {
-    ONLOAD_TRIGGER,
-    IF_LESS_THAN,
-    LOOP_FROM_TO,
-    VAR_DECLARE,//does not do anything
-    PLACE_BLOCK,
-    PLACE_PLAYER,
-    ONTICK_TRIGGER,
-    MOVE_PLAYER_BY,
-    IF_EQUAL_TO,
-    IF_GREATER_THAN,
-    IF_NOT_EQUAL_TO
+    ONLOAD_TRIGGER("On Load"),
+    IF_LESS_THAN("If Less Than"),
+    LOOP_FROM_TO("For"),
+    VAR_DECLARE("Var"),//does not do anything
+    PLACE_BLOCK("Place Block"),
+    PLACE_PLAYER("Place Player"),
+    ONTICK_TRIGGER("On Tick"),
+    MOVE_PLAYER_BY("Move Player"),
+    IF_EQUAL_TO("If Equal To"),
+    IF_GREATER_THAN("If Greater Than"),
+    IF_NOT_EQUAL_TO("If Not Equal To");
+
+    String s;
+
+    BlockTypes(String string) {
+        s = string;
+    }
 }

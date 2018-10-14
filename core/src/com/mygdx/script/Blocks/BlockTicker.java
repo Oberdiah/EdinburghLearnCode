a9048f4734b20d75ce05baec616225f8d50805e8
AlliterativeAnchovies <alliterative.anchovies@gmail.com>
2018-10-14 11:22:34 +0100

    Fixed potential bug

--- core/src/com/mygdx/script/Blocks/BlockTicker.java
+++ core/src/com/mygdx/script/Blocks/BlockTicker.java
@@ -2,6 +2,7 @@ package com.mygdx.script.Blocks;
 
 public class BlockTicker extends Block {
     public String ticker;
+    public boolean classwise = false;
     BlockTicker(String t) {
         ticker = t;
     }
